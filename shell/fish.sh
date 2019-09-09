596cfd0c4f08cfc082fa922bf2f7282843c53de6
AodhanDalton <39493056+AodhanDalton@users.noreply.github.com>
2019-09-09 09:40:49 +0100

    Update fish.sh

--- shell/fish.sh
+++ shell/fish.sh
@@ -1,9 +1,3 @@
 #installing fish
 sudo apt-get install fish -y
 fish
-chsh -s /usr/bin/fish
-sudo mkdir -p ~/.config/fish
-
-#installing omf & setting theme
-curl -L https://get.oh-my.fish | fish
-omf install lambda
