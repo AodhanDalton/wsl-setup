4a851ebf396b075b5f91063d8e3c8a66cf10da8c
AodhanDalton <39493056+AodhanDalton@users.noreply.github.com>
2019-09-09 10:17:01 +0100

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -4,6 +4,7 @@ sudo apt upgrade -y
 sudo apt install docker python python-pip docker.io unzip -y
 sudo pip install awscli
 sudo apt install direnv
+wslconfig.exe  /s ubuntu
 
 #creating an environmental variable for the user profile
 setx.exe WSLENV "USERPROFILE/p"
