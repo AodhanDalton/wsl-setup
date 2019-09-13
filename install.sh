8c4e85c6838773c33f34a31383243fd80e36fa9d
AodhanDalton <39493056+AodhanDalton@users.noreply.github.com>
2019-09-13 12:03:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd ~/.wsl-setup
 ./extras/terraform.sh &&
 ./extras/terragrunt.sh
 
-cat ./conf-scripts/fish.conf > ~/.config/fish/fish.conf
+cat ./conf-scripts/fish.conf > ~/.config/fish/conf.d/fish.conf
 sudo sh -c 'cat ./conf-scripts/wsl.conf > /etc/wsl.conf'
 
 echo "¬(0_0¬) Installed successfully ¬(0_0¬)"
