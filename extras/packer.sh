0aef188af2746c0fe27fc3c4b01cc3ccbb2fe07d
AodhanDalton <39493056+AodhanDalton@users.noreply.github.com>
2019-09-17 09:39:32 +0100

    Update packer.sh

--- extras/packer.sh
+++ extras/packer.sh
@@ -1,3 +1,4 @@
 curl -slO https://releases.hashicorp.com/packer/1.4.3/packer_1.4.3_linux_amd64.zip
 unzip packer_1.4.3_linux_amd64.zip
 sudo mv packer /usr/local/bin
+rm -r packer_1.4.3_linux_amd64.zip
