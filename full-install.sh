e97df1a0a1cda42cc21727695c0c63c52d6c30d5
Aodhan Dalton <aodhan.dalton@rewardinsight.com>
2019-10-08 16:21:22 +0100

    full install edited

--- full-install.sh
+++ full-install.sh
@@ -1,23 +1,53 @@
+#######################################################################
+#                       Aodhan Dalton
+#                   Full wsl setup v 0.3
+#######################################################################
+
 #!/bin/sh
-set -e
+set -x
 
 # Install basic packages
 sudo apt update
 sudo apt upgrade -y
-sudo apt install docker python python-pip docker.io unzip -y
+
+#break
+#check if the packages are previously installed if not install them
+dpkg -l | grep -qw docker || sudo apt-get install docker -y
+dpkg -l | grep -qw docker.io || sudo apt-get install docker.io -y
+dpkg -l | grep -qw python || sudo apt-get install python -y
+dpkg -l | grep -qw python-pip || sudo apt-get install python-pip -y
+dpkg -l | grep -qw unzip || sudo apt-get install unzip -y
+dpkg -l | grep -qw direnv || sudo apt-get install direnv -y
+
+#break
+
 sudo pip install awscli
-sudo apt install direnv
-wslconfig.exe  /s ubuntu
+
+#break
+
+#run wslconfig.exe /l to get a list of all installed distros
+wslconfig.exe  /s legacy
 
 # Creating an environmental variable for the user profile
 setx.exe WSLENV "USERPROFILE/p"
 
 # Installing fish
 sudo apt-get install fish -y
+
+#break
+
 fish
 
 chsh -s /usr/bin/fish
-sudo mkdir -p ~/.config/fish
+#break
+
+dir=~/.config/fish
+if [ ! -d $dir ]
+then
+     mkdir $dir
+else
+     echo "Directory exists"  
+fi
 
 # Installing omf & setting theme
 curl -L https://get.oh-my.fish | fish
@@ -41,7 +71,7 @@ sudo mv packer /usr/local/bin
 rm -r packer_1.4.3_linux_amd64.zip
 
 # Alias file for fish
-cat 'direnv hook fish | source
+echo 'direnv hook fish | source
 export AWS_SHARED_CREDENTIALS_FILE=$USERPROFILE/.aws/credentials
 alias gimme-aws-creds="docker run --rm -it -v $USERPROFILE/.aws/credentials:/root/.aws/credentials -v $USERPROFILE/.okta_aws_login_config:/root/.okta_aws_login_config rewardinsight/gimme-aws-creds:v1.1.5"
 #General
@@ -50,18 +80,21 @@ alias projects="cd /c/projects"
 alias dropbox="cd ~/dropbox"
 alias sapt="sudo apt install"
 #Directory movemenet aliases
-alias ..='cd ..'
-alias ...='cd ../../../'
-alias ....='cd ../../../../'
-alias .....='cd ../../../../'
+alias ..="cd .."
+alias ...="cd ../../../"
+alias ....="cd ../../../../"
+alias .....="cd ../../../../"
 alias back="cd -"
-alias user="cd /c/Users/{User}"
+alias l="ls -al"
+alias user="cd $USERPROFILE"
 export DOCKER_HOST=tcp://localhost:2375
 
-if test -f /home/aodhan/.autojump/share/autojump/autojump.fish; . /home/aodhan/.autojump/share/autojump/autojump.fish; end' > ~/.config/fish/conf.d/fish.conf
+if test -f /home/aodhan/.autojump/share/autojump/autojump.fish; . /home/aodhan/.autojump/share/autojump/autojump.fish; end' | sudo tee ~/.config/fish/config.fish
+
+source  ~/.config/fish/config.fish
 
 # Wsl configuration file to set c as root instead of mnt
-cat '# Enable extra metadata options by default
+echo '# Enable extra metadata options by default
 [automount]
 enabled = true
 root = /
@@ -71,10 +104,10 @@ mountFsTab = false
 # Enable DNS – even though these are turned on by default, we’ll specify here just to be explicit.
 [network]
 generateHosts = true
-generateResolvConf = true' > /etc/wsl.conf
+generateResolvConf = true' | sudo tee /etc/wsl.conf
 
 
-echo "¬(0_0¬) Installed successfully ¬(0_0¬)"
+echo "¬(*_*¬) Installed successfully ¬(*_*¬)"
 
 exit 0
 
